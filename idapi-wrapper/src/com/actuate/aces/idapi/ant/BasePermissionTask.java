5eeec82eda1fda12296088cdffc7eaa3f7c0f523
Gary Xue <gxue@actuate.com>
2014-07-01 17:32:49 -0400

    updated Ant tasks for iHub3

---
+++ idapi-wrapper/src/com/actuate/aces/idapi/ant/BasePermissionTask.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2014 Actuate Corporation
+ */
+
+package com.actuate.aces.idapi.ant;
+
+import java.util.ArrayList;
+import org.apache.tools.ant.Task;
+import com.actuate.schemas.ArrayOfPermission;
+import com.actuate.schemas.Permission;
+
+/**
+ * Base task class for several IDAPI tasks that sets user/usergroup permissions
+ */
+public abstract class BasePermissionTask extends Task {
+	protected ArrayList<Permission> permissions = new ArrayList<Permission>();
+	
+	public void setUserPermissions(String userPermissions) {
+		if (userPermissions != null) {
+			String[] perms = userPermissions.split(",");
+			for (int x = 0; x < perms.length; x++) {
+				int colon = perms[x].indexOf(":");
+				if (colon > 0) {
+					String userName = perms[x].substring(0, colon);
+					String rights = perms[x].substring(colon+1);
+					Permission p = new Permission();
+					p.setUserName(userName);
+					p.setAccessRight(rights);
+					permissions.add(p);
+				}
+			}
+		}
+	}
+
+	/**
+	 *  @deprecated Use setUserGroupPermission instead; kept for backward compatibility
+	 */
+	public void setRolePermissions(String rolePermissions) {
+		setUserGroupPermissions(rolePermissions);
+	}
+
+	public void setUserGroupPermissions(String userGroupPermissions) {
+		if (userGroupPermissions != null) {
+			String[] perms = userGroupPermissions.split(",");
+			for (int x = 0; x < perms.length; x++) {
+				int colon = perms[x].indexOf(":");
+				if (colon > 0) {
+					String groupName = perms[x].substring(0, colon);
+					String rights = perms[x].substring(colon+1);
+					Permission p = new Permission();
+					p.setUserGroupName(groupName);
+					p.setAccessRight(rights);
+					permissions.add(p);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * @return Permission array. NULL if no user or user group permission is set for task
+	 */
+	protected ArrayOfPermission getArrayOfPermissions() {
+		if (permissions.size() == 0 )
+			return null;
+		else{
+			Permission[] perms = permissions.toArray(new Permission[0]);
+
+for (int i =0; i < perms.length; i++){
+	System.out.println("Rights:  " + perms[i].getAccessRight());
+}
+			ArrayOfPermission aop = new ArrayOfPermission(perms);
+			return aop;
+		}
+	}
+
+}
--- idapi-wrapper/src/com/actuate/aces/idapi/ant/IdapiVolumeUploadSimple.java
+++ idapi-wrapper/src/com/actuate/aces/idapi/ant/IdapiVolumeUploadSimple.java
@@ -6,59 +6,29 @@ package com.actuate.aces.idapi.ant;
 
 import com.actuate.aces.idapi.actions.VolumeUploadSimple;
 import com.actuate.schemas.ArrayOfPermission;
-import com.actuate.schemas.Permission;
 import org.apache.tools.ant.BuildException;
-import org.apache.tools.ant.Task;
 
 import javax.xml.rpc.ServiceException;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.util.Vector;
 
-public class IdapiVolumeUploadSimple extends Task {
+public class IdapiVolumeUploadSimple extends BasePermissionTask {
 	private String serverUrl;
 	private String authId;
 	private String sourcePath;
 	private String targetPath;
-	private String userPermissions;
-	private String rolePermissions;
 	private boolean deleteFirst = false;
 	private boolean recursive = true;
 
 	public void execute() throws BuildException {
 		if (serverUrl == null || authId == null || sourcePath == null || targetPath == null) {
-			throw new BuildException("\r\n\r\nUSAGE ERROR:  IdapiVolumeUploadSimple requires AuthId, ServerUrl, " + "\r\n              SourcePath, TargetPath attributes." + "\r\nOptional attributes are UserPermissions, RolePermissions, DeleteFirst, and Recursive." + "\r\nExample:" + "\r\n\t<IdapiVolumeUploadSimple AuthId=\"${AuthId}\" " + "\r\n\t                         ServerUrl=\"${ServerUrl}\" " + "\r\n\t                         SourcePath=\"C:/my directory\" " + "\r\n\t                         TargetPath=\"/Public\" " + "\r\n\t                         UserPermissions=\"/Public\" " + "\r\n\t                         RolePermissions=\"/Public\" " + "\r\n\t                         DeleteFirst=\"true\" " + "\r\n\t                         Recursive=\"false\" />");
+			throw new BuildException("\r\n\r\nUSAGE ERROR:  IdapiVolumeUploadSimple requires AuthId, ServerUrl, " + "\r\n              SourcePath, TargetPath attributes." + "\r\nOptional attributes are UserPermissions, UserGroupPermissions, DeleteFirst, and Recursive." + "\r\nExample:" + "\r\n\t<IdapiVolumeUploadSimple AuthId=\"${AuthId}\" " + "\r\n\t                         ServerUrl=\"${ServerUrl}\" " + "\r\n\t                         SourcePath=\"C:/my directory\" " + "\r\n\t                         TargetPath=\"/Public\" " + "\r\n\t                         UserPermissions=\"/Public\" " + "\r\n\t                         RolePermissions=\"/Public\" " + "\r\n\t                         DeleteFirst=\"true\" " + "\r\n\t                         Recursive=\"false\" />");
 		}
 		try {
 			VolumeUploadSimple vus = new VolumeUploadSimple(serverUrl, authId);
 
-			Vector<Permission> vPermissions = null;
-			if (userPermissions != null) {
-				vPermissions = new Vector<Permission>();
-				if (userPermissions.indexOf(",") != -1) {
-					String[] userPerms = userPermissions.split(",");
-					for (int x = 0; x < userPerms.length; x++) {
-						vPermissions.add(new Permission(null, userPerms[x].substring(0, userPerms[x].indexOf(":")), null, null, userPerms[x].substring(userPerms[x].indexOf(":") + 1)));
-					}
-				} else {
-					vPermissions.add(new Permission(null, userPermissions.substring(0, userPermissions.indexOf(":")), null, null, userPermissions.substring(userPermissions.indexOf(":") + 1)));
-				}
-			}
-			if (rolePermissions != null) {
-				if (vPermissions == null)
-					vPermissions = new Vector<Permission>();
-				if (rolePermissions.indexOf(",") != -1) {
-					String[] rolePerms = rolePermissions.split(",");
-					for (int x = 0; x < rolePerms.length; x++) {
-						vPermissions.add(new Permission(rolePerms[x].substring(0, rolePerms[x].indexOf(":")), null, null, null, rolePerms[x].substring(rolePerms[x].indexOf(":") + 1)));
-					}
-				} else {
-					vPermissions.add(new Permission(rolePermissions.substring(0, rolePermissions.indexOf(":")), null, null, null, rolePermissions.substring(rolePermissions.indexOf(":") + 1)));
-				}
-			}
-			if (vPermissions != null) {
-				Permission[] permissions = new Permission[vPermissions.size()];
-				ArrayOfPermission aop = new ArrayOfPermission(vPermissions.toArray(permissions));
+			ArrayOfPermission aop = getArrayOfPermissions();
+			if (aop != null) {
 				vus.setPermissions(aop);
 			}
 			vus.upload(sourcePath, targetPath, deleteFirst, recursive);
@@ -85,14 +55,6 @@ public class IdapiVolumeUploadSimple extends Task {
 
 	public void setTargetPath(String targetPath) {
 		this.targetPath = targetPath;
-	}
-
-	public void setUserPermissions(String userPermissions) {
-		this.userPermissions = userPermissions;
-	}
-
-	public void setRolePermissions(String rolePermissions) {
-		this.rolePermissions = rolePermissions;
 	}
 
 	public void setDeleteFirst(String deleteFirst) {
